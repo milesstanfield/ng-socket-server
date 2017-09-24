56adfb24afa9da03dd2c2ea95cb4374528dd16d8
Miles <milesstanfield@gmail.com>
2017-09-24 00:02:18 -0400

    added boilerplate server.js file

---
+++ server.js
@@ -0,0 +1,33 @@
+var express        = require('express');
+var methodOverride = require('method-override');
+var path           = require('path');
+var compress       = require('compression');
+var app            = express();
+var port           = process.env.PORT || 3000;
+var http           = require('http').createServer(app);
+var io             = require('socket.io')(http);
+
+// gzip
+app.use(compress());
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
+// set the static files location
+app.use(express.static(__dirname + '/dist'));
+// route to handle all angular requests
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/dist/index.html'));
+});
+
+io.on('connection', function (socket) {
+  socket.on('disconnect', function() { });
+
+  socket.on('add-message', function(message) {
+    io.emit('message', { text: message });
+  });
+});
+
+http.listen(port, function() {
+  if (app.get('env') === 'development') {
+    console.log('Magic happening at ' + 'http://localhost:' + port);
+  }
+});
